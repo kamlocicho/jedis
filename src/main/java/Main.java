bc8914fb7dd88ba7bb27d5e0fad3ee70d7d8f131
kamlocicho <orwat.kamil3@gmail.com>
2024-05-06 14:05:39 +0200

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,54 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
 
 
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
         int port = 6379;
         try {
-          serverSocket = new ServerSocket(port);
-          // Since the tester restarts your program quite often, setting SO_REUSEADDR
-          // ensures that we don't run into 'Address already in use' errors
-          serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
-          clientSocket = serverSocket.accept();
-        } catch (IOException e) {
-          System.out.println("IOException: " + e.getMessage());
-        } finally {
-          try {
-            if (clientSocket != null) {
-              clientSocket.close();
+            serverSocket = new ServerSocket(port);
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
+            // Wait for connection from client.
+            clientSocket = serverSocket.accept();
+            System.out.println("New connection accepted.");
+
+            InputStream input = clientSocket.getInputStream();
+            OutputStream output = clientSocket.getOutputStream();
+
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(input));
+
+            inputReader.readLine();
+            inputReader.readLine();
+
+            String command = inputReader.readLine();
+            System.out.println("Received command: " + command);
+
+            if(Objects.equals(command, "PING")) {
+                String res = "+PONG\r\n";
+                output.write(res.getBytes(StandardCharsets.UTF_8));
+                output.close();
             }
-          } catch (IOException e) {
+
+        } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
-          }
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
         }
-  }
+    }
 }
